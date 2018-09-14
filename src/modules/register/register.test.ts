0d78608a355484f34b55d78fea5c4642a42bf8a4
tylergreulich <tgreul6@gmail.com>
2018-09-14 12:57:49 -0700

    tests

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,26 +1,24 @@
+import { startServer } from './../../server/startServer';
 import { User } from './../../interfaces/auth.interface';
 import { schema } from './../../server/createSchema';
 import {
   startTestConnection,
   dropTestConnection
 } from './../../server/connectToMongoDB';
-import { gql } from 'apollo-server-express';
-import request from 'supertest';
+import { gql, UserInputError } from 'apollo-server-express';
+import { request } from 'graphql-request';
 
-const url = 'http://localhost:4000/';
-const fakeApiRequest = request(url);
+const username = 'bob';
+const email = 'bob@bob.com';
+const password = 'bobbob';
+const confirmPassword = 'bobbob';
 
-const u = '';
-const e = '';
-const p = '';
-const cP = '';
-
-const registerMutation = gql`
-  mutation RegisterUser($username: String!, email: String!, password: String!, confirmPassword: String!) {
-    registerUser(username: ${u}, email: ${e}, password: ${p}, confirmPassword: ${cP}) {
-      token
-    }
+const mutation = (u: string, e: string, p: string, cP: string) => `
+mutation {
+  registerUser(username: "${u}", email: "${e}", password: "${p}", confirmPassword: "${cP}") {
+    token
   }
+}
 `;
 
 describe('loginResolver', () => {
@@ -28,12 +26,15 @@ describe('loginResolver', () => {
 
   afterEach(() => dropTestConnection());
 
-  describe('loginUser', () => {
+  describe('registerUser', () => {
     it('Should register a user', async () => {
-      const results = await fakeApiRequest
-        .post('/graphql')
-        .send({ mutation: registerMutation });
-      expect(results).toBe(400);
+      const { registerUser } = await request(
+        'http://localhost:4000/graphql',
+        mutation(username, email, password, confirmPassword)
+      );
+
+      expect(registerUser).not.toBeUndefined();
+      expect(typeof 'registerUser.token').toBe('string');
     });
   });
 });
