58a26b14a576b7c3f9fa0f629860a92fe5bbc614
tylergreulich <tgreul6@gmail.com>
2018-09-28 14:53:28 -0700

    route transitions

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,6 +12,7 @@ import { HomePage } from 'src/screens/Home/ScreensHomePage';
 import MoviePage from 'src/screens/Movie/ScreensMoviePage';
 import { BrowsePage } from 'src/screens/Movie/ScreensBrowsePage';
 import Navigation from 'src/screens/Navigation/ScreensNavigation';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 // import { withSession } from 'src/utils/withSession';
 
@@ -35,17 +36,53 @@ const client = new ApolloClient({
   }
 });
 
+// const locationPaths = {
+//   home: '/',
+//   browseMovies: '/browse',
+//   viewMovie: '/browse/movie/:id'
+// };
+
 const Root = () => (
   <BrowserRouter>
-    <>
-      <Route exact={true} path="/" component={HomePage} />
-      <Navigation />
-      <Switch>
-        <Route exact={true} path="/browse" component={BrowsePage} />
-        <Route exact={true} path="/browse/movie/:id" component={MoviePage} />
-        <Redirect to="/" />
-      </Switch>
-    </>
+    <Route
+      render={({ location }) =>
+        console.log(location.pathname) || (
+          <>
+            <Navigation />
+            <TransitionGroup>
+              <CSSTransition
+                timeout={
+                  location.pathname === '/'
+                    ? 250
+                    : location.pathname === '/browse'
+                      ? 1000
+                      : 0
+                }
+                classNames={
+                  location.pathname === '/'
+                    ? 'slide'
+                    : location.pathname === '/browse'
+                      ? 'browse'
+                      : 'slide'
+                }
+                key={location.key}
+              >
+                <Switch location={location}>
+                  <Route exact={true} path="/" component={HomePage} />
+                  <Route exact={true} path="/browse" component={BrowsePage} />
+                  <Route
+                    exact={true}
+                    path="/browse/movie/:id"
+                    component={MoviePage}
+                  />
+                  <Redirect to="/" />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          </>
+        )
+      }
+    />
   </BrowserRouter>
 );
 
